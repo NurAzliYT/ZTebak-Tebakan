cbbe90abda4774790ad485e68569015e7b0de272
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-09-08 11:42:45 +0700

    Wee

--- app/src/main/java/id/nurazlib/ztebaktebakan/Question.java
+++ app/src/main/java/id/nurazlib/ztebaktebakan/Question.java
@@ -4,11 +4,13 @@ public class Question {
     private String questionText;
     private String[] options;
     private String correctAnswer;
+    private String hint;
 
     public Question(String questionText, String[] options, String correctAnswer) {
         this.questionText = questionText;
         this.options = options;
         this.correctAnswer = correctAnswer;
+        this.hint = hint;
     }
 
     public String getQuestionText() {
@@ -21,5 +23,9 @@ public class Question {
 
     public String getCorrectAnswer() {
         return correctAnswer;
+    }
+
+    public String getHint() {
+        return hint;
     }
 }
